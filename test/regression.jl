069e01f34a67709962b5891d3024634e97043b3d
Utkarsh <rajpututkarsh530@gmail.com>
2024-02-14 12:00:40 -0500

    Update tests

--- test/regression.jl
+++ test/regression.jl
@@ -1,24 +1,30 @@
-using Optimization, StaticArrays, SimpleOptimization
+using Optimization, StaticArrays, SimpleOptimization, Test
 
-N = 2
-
-function rosenbrock(x, p)
-    sum(p[2] * (x[i + 1] - x[i]^2)^2 + (p[1] - x[i])^2 for i in 1:(length(x) - 1))
+function objf(x, p)
+    return x[1]^2 + x[2]^2
 end
 
-x0 = @SArray zeros(Float32, N)
-p = @SArray Float32[1.0, 100.0]
+using ForwardDiff
+
+x0 = @SArray ones(Float32, 2)
+l1 = objf(x0, nothing)
 
 using ForwardDiff
 optf = OptimizationFunction(rosenbrock, Optimization.AutoForwardDiff())
 prob = OptimizationProblem(optf, x0, p)
 
 sol = solve(prob, SimpleLBFGS())
+@test 10 * sol.objective < l1
+
 sol = solve(prob, SimpleBFGS())
+@test 10 * sol.objective < l1
 
 using Enzyme
 optf = OptimizationFunction(rosenbrock, Optimization.AutoEnzyme())
 prob = OptimizationProblem(optf, x0, p)
 
 sol = solve(prob, SimpleLBFGS())
+@test 10 * sol.objective < l1
+
 sol = solve(prob, SimpleBFGS())
+@test 10 * sol.objective < l1
